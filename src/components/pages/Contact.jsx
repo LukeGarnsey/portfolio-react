fec5ad336f40d84b2aa776f8fdef728669aac4d7
Luke Garnsey <lukegarnsey@gmail.com>
2024-03-03 22:29:36 -0500

    Contact form added

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
 export default function Contact(){
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const handleChange = (e) =>{
+    const { name, value} = e.target;
+    setFormData({ ...formData, [name]: value});
+  };
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    try{
+      const response = await fetch('/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'aplication/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      if(response.ok){
+        console.log('Email Sent');
+        setFormData({name:'', email:'', message:''});
+      }else{
+        console.log('Failed to send email');
+      }
+    }catch(error){
+      console.error('Error:', error);
+    }
+  }
+
   return (
     <div>
-      <h1>Contact</h1>
+      <h3>About me</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Your Name" required />
+        <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Your Email" required />
+        <textarea name="message" value={formData.message} onChange={handleChange} placeholder="Your Message" rows="4" required />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
