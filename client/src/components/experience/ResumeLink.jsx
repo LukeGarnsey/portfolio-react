809f754c20ee8883b7ad7c6196829b64edefd1a9
Luke Garnsey <lukegarnsey@gmail.com>
2024-05-19 13:40:46 -0400

    update

--- client/src/components/experience/ResumeLink.jsx
+++ client/src/components/experience/ResumeLink.jsx
@@ -5,7 +5,7 @@ export default function ResumeLink(){
   return(
     <>
       <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
-      href="/Resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in new tab)">
+      href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in new tab)">
         <span>View full <span className="inline-block"></span>Résumé <IconAnimate /></span>
       </a>
     </>
